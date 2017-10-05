19f2c3c4d3f9533e0e09780ff47babfac4b8bb10
Min Kim <mk4555@nyu.edu>
2017-10-05 00:45:22 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,3 +23,7 @@ end
 def position_taken?(board, index)
   !(board[index] == " " || board[index] == "" || board[index] == nil)
 end
+
+def valid_move?(board, index)
+  position_taken?(board, index)
+end
