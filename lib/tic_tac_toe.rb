15c5d1b09c6b2503dd7801904c3bcf617a1dd48a
Min Kim <mk4555@nyu.edu>
2017-10-05 01:55:38 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -102,12 +102,18 @@ def winner(board)
 end
 
 def play(board)
+  display_board(board)
   puts "Please enter 1-9:"
   input = gets
   input = input.chomp
   input = input_to_index(input)
   counter = 0
   turn(board)
+  counter = turn_count(board)
+  until counter < 8 do
+    turn(board)
+    
+  end
   # until counter < 9 do
   #   display_board(board)
   #
